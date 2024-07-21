611817eb0149dc258afc6e641b83d5ce89b77929
rohim48 <noerrohim10@gmail.com>
2024-07-21 13:58:30 +0700

    added contact input

---
+++ src/components/ContactInput.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+export class ContactInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      tag: "",
+    };
+
+    this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
+    this.onTagChangeEventHandler = this.onTagChangeEventHandler.bind(this);
+    this.onSubmitChangeEventHandler = this.onSubmitChangeEventHandler.bind(this);
+  }
+
+  onNameChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        name: event.target.value,
+      }
+    });
+  }
+
+  onTagChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        tag: event.target.value,
+      }
+    });
+  }
+
+  onSubmitChangeEventHandler(event) {
+    event.preventDefault();
+    this.props.addContact(this.state);
+  }
+
+  render() {
+    return (
+      <form className="contact-input" onSubmit={this.onSubmitChangeEventHandler}>
+        <input
+          type="text"
+          placeholder="Nama"
+          value={this.state.name}
+          onChange={this.onNameChangeEventHandler}
+        />
+        <input
+          type="text"
+          placeholder="Tag"
+          value={this.state.tag}
+          onChange={this.onTagChangeEventHandler}
+        />
+        <button type="submit">Tambah</button>
+      </form>
+    );
+  }
+}
+
+export default ContactInput;
