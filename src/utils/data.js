909d4c64477f2f9d67baa9efb61a547665cd1f28
rohim48 <noerrohim10@gmail.com>
2024-07-21 12:14:28 +0700

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Dimas Saputra',
+            tag: 'dimasdm',
+            imageUrl: './public/images/dimasmds.jpeg',
+        },
+        {
+            id: 2,
+            name: 'Arif Faizin',
+            tag: 'arifaizin',
+            imageUrl: '/public/images/arifaizin.jpeg',
+        },
+        {
+            id: 3,
+            name: 'Rahmat Fajri',
+            tag: 'rfajri',
+            imageUrl: './public/images/rfajri27.jpeg',
+        },
+    ];
+}
+
+export { getData };
